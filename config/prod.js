cec7ed87cb12fc8a5ce9f1d0b9fc13856b44b38b
Stefan Tien <stefan55831@gmail.com>
2017-09-14 16:00:03 +0800

    Remove some information

--- config/prod.js
+++ config/prod.js
@@ -7,9 +7,3 @@ module.exports = {
   cookieKey1: process.env.COOKIE_KEY_1,
   cookieKey2: process.env.COOKIE_KEY_2
 };
-
-// 40488498310-0ttrmm98c3jap5o5tc2p648jeno5dt1p.apps.googleusercontent.com
-// H9mMfA_sDg0SLCUHy1sMSjlf
-// facebook: 2021756004735116
-// facebook password: 23b7215978ba44e6211058c8298f4db1
-// mongodb://<dbuser>:<dbpassword>@ds119044.mlab.com:19044/roommate-prod
