3929c790f84599adae3128e1a4c2121583d6798e
Stefan Tien <stefan55831@gmail.com>
2017-09-14 14:15:14 +0800

    Set deployment environment

---
+++ config/prod.js
@@ -0,0 +1,15 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookAppID: process.env.FACEBOOK_APP_ID,
+  facebookAppSecret: process.env.FACEBOOK_APP_SECRET,
+  mongodbURI: process.env.MONGODB_URI,
+  cookieKey1: process.env.COOKIE_KEY_1,
+  cookieKey2: process.env.COOKIE_KEY_2
+};
+
+// 40488498310-0ttrmm98c3jap5o5tc2p648jeno5dt1p.apps.googleusercontent.com
+// H9mMfA_sDg0SLCUHy1sMSjlf
+// facebook: 2021756004735116
+// facebook password: 23b7215978ba44e6211058c8298f4db1
+// mongodb://<dbuser>:<dbpassword>@ds119044.mlab.com:19044/roommate-prod
